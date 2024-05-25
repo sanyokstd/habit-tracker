397cad2820768c872ec9d1034172c017f276e97e
Olexandr Stadnik <59456430+sanyokstd@users.noreply.github.com>
2024-05-26 02:11:24 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { userStore } from './src/store/store';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const {user,setUser} = userStore();
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View>
+        <Text>{user}</Text>
+        <Button title="Click me" onPress={() => setUser('new user 2')} />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
